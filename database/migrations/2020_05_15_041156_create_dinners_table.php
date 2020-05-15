f01fc2eb70d6ffd6e46b7cac2901c6a8a3f2c9ae
Darrell Weaver <darrellweaverjr@gmail.com>
2020-05-15 00:23:25 -0700

    database migration for food and drink menu

---
+++ database/migrations/2020_05_15_041156_create_dinners_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDinnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dinners', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->string('name');
+            $table->string('desc');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dinners');
+    }
+}
