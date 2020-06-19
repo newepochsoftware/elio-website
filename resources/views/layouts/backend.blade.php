d244707c99bb858ed08a1fc4efee2c8a28bbb524
Darrell Weaver <darrellweaverjr@gmail.com>
2020-06-19 12:23:12 -0700

    fixed header style for text

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -22,9 +22,9 @@
         <link rel="shortcut icon" type="image/x-icon" href="https://storage.googleapis.com/wynn-bucket/favicon.ico">
 
         <!-- App css -->
-        <link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
     </head>
 
@@ -602,27 +602,27 @@
         <div class="rightbar-overlay"></div>
 
         <!-- Vendor js -->
-        <script src="{{ asset('admin/assets/js/vendor.min.js') }}"></script>
+        <script src="{{ asset('/admin/assets/js/vendor.min.js') }}"></script>
 
 
         <!-- Flot chart -->
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.time.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.tooltip.min.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.resize.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.pie.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.crosshair.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/curvedLines.js') }}"></script>
-        <script src="{{ asset('admin/assets/libs/flot-charts/jquery.flot.axislabels.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.time.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.tooltip.min.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.resize.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.pie.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.crosshair.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/curvedLines.js') }}"></script>
+        <script src="{{ asset('/admin/assets/libs/flot-charts/jquery.flot.axislabels.js') }}"></script>
 
          <!-- KNOB JS -->
-         <script src="{{ asset('admin/assets/libs/jquery-knob/jquery.knob.min.js') }}"></script>
+         <script src="{{ asset('/admin/assets/libs/jquery-knob/jquery.knob.min.js') }}"></script>
 
-         <script src="{{ asset('admin/assets/js/pages/dashboard.init.js') }}"></script>
+         <script src="{{ asset('/admin/assets/js/pages/dashboard.init.js') }}"></script>
 
 
         <!-- App js -->
-        <script src="{{ asset('admin/assets/js/app.min.js') }}"></script>
+        <script src="{{ asset('/admin/assets/js/app.min.js') }}"></script>
 
     </body>
 </html>
