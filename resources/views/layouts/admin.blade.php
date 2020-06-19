d38ca0b8967879cfe04661b81d18bcfcb6c6c0ef
Darrell Weaver <darrellweaverjr@gmail.com>
2020-06-19 12:21:48 -0700

    updated admin paths to css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,9 +23,9 @@
 
 
         <!-- App css -->
-        <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Vendor js -->
 
 
